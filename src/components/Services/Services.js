2a72fb4c17f73137402c24966a673aa29bd28186
David Na <hna206@naver.com>
2023-03-20 03:49:59 +1300

    component - qualification

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -61,7 +61,7 @@ const Services = () => {
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    Using React.js as my WebApp Language
+                    Simple Emotional Diary
                   </p>
                 </li>
 
