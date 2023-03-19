4a3a0519bd582eb9911a068258fe674741c572a2
David Na <hna206@naver.com>
2023-03-20 03:01:20 +1300

    FE -

--- src/components/Skills/Frontend.js
+++ src/components/Skills/Frontend.js
@@ -3,7 +3,7 @@ import React from "react";
 const Frontend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Frontend developer</h3>
+      <h3 className="skills__title">Frontend</h3>
 
       <div className="skills__box">
         <div className="skills__group">
@@ -11,8 +11,8 @@ const Frontend = () => {
             <i class="bx bx-badge-check"></i>
 
             <div>
-              <h3 className="skills__name">HTML</h3>
-              <span className="skills__level">Basic</span>
+              <h3 className="skills__name">HTML5</h3>
+              {/* <span className="skills__level">Advanced</span> */}
             </div>
           </div>
 
@@ -20,8 +20,8 @@ const Frontend = () => {
             <i class="bx bx-badge-check"></i>
 
             <div>
-              <h3 className="skills__name">CSS</h3>
-              <span className="skills__level">Advanced</span>
+              <h3 className="skills__name">CSS3</h3>
+              {/* <span className="skills__level">Intermediate</span> */}
             </div>
           </div>
 
@@ -30,7 +30,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">JavaScript</h3>
-              <span className="skills__level">Intermediate</span>
+              {/* <span className="skills__level">Intermediate</span> */}
             </div>
           </div>
         </div>
@@ -41,7 +41,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">Bootstrap</h3>
-              <span className="skills__level">Intermediate</span>
+              {/* <span className="skills__level">Intermediate</span> */}
             </div>
           </div>
 
@@ -50,7 +50,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">Git</h3>
-              <span className="skills__level">Intermediate</span>
+              {/* <span className="skills__level">Intermediate</span> */}
             </div>
           </div>
 
@@ -59,7 +59,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
+              {/* <span className="skills__level">Intermediate</span> */}
             </div>
           </div>
         </div>
