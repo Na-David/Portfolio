764e452c41c6e6e31f3aa975e2c1cc02d19b7b2b
David Na <hna206@naver.com>
2023-03-20 02:07:47 +1300

    Home css

---
+++ src/components/Home/Data.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className='home__data'>
+      <h1 className='home__title'>David Na</h1>
+      <h3 className='home__subtitle'>Last year of studying in Software Development at AUT</h3>
+      <p className='home__description'> 
+        I am a student who has regularly worked with React.js and C# in learning software development.
+        I enjoy programming and have a passion for solving problems with perseverance. 
+        Furthermore, I strongly desire to learn new technologies and am highly motivated to put forth the effort required to learn. 
+      </p>
+
+      <a href='#contact' className='button button--flex'>
+        Say Hello :D
+      </a><i className='uil uil-message nav__icon'></i>
+    </div>
+  )
+}
+
+export default Data
