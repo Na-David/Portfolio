29b1f695ae488d2a46492c95ac43781a597903c6
David Na <hna206@naver.com>
2023-03-20 02:14:59 +1300

    home - data

--- src/components/Home/Data.js
+++ src/components/Home/Data.js
@@ -4,7 +4,7 @@ const Data = () => {
   return (
     <div className='home__data'>
       <h1 className='home__title'>David Na</h1>
-      <h3 className='home__subtitle'>Last year of studying in Software Development at AUT</h3>
+      <h3 className='home__subtitle'>AUT BCIS student<br />Software Development</h3>
       <p className='home__description'> 
         I am a student who has regularly worked with React.js and C# in learning software development.
         I enjoy programming and have a passion for solving problems with perseverance. 
