d8819b3d86db330702265028d3030ea55aba0dfc
David Na <hna206@naver.com>
2023-03-20 14:04:29 +1300

    href

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <h1 className="footer__title">David</h1>
+        <h1 className="footer__title">David Na</h1>
 
         <ul className="footer__list">
           <li>
@@ -21,7 +21,7 @@ const Footer = () => {
           </li>
 
           <li>
-            <a href="#qualification" className="footer__link">
+            <a href="#Qualification" className="footer__link">
               Qualification
             </a>
           </li>
