f269f6deb6f8c520d35c48e91871892ea19ca9f1
David Na <hna206@naver.com>
2023-03-20 14:12:30 +1300

    details changed

--- src/components/About/Info.js
+++ src/components/About/Info.js
@@ -7,24 +7,14 @@ const Info = () => {
         <i class="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Studying</h3>
-        <span className="about__subtitle">4+ Years Learning</span>
+        <span className="about__subtitle">4+ Years of Learning</span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
 
         <h3 className="about__title">Achieved</h3>
-        <span className="about__subtitle">
-          <ul>
-            <li>
-            NZ Diploma in IT Technical Support Level 5
-            </li>
-            <li>
-            NZ Diploma in Software Development Level 6
-
-            </li>
-          </ul>
-        </span>
+        <span className="about__subtitle">6+ Small Projects</span>
       </div>
 
       <div className="about__box">
--- src/components/ScrollUp/ScrollUp.js
+++ src/components/ScrollUp/ScrollUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Scrollup.css";
+import "./ScrollUp.css";
 
 const ScrollUp = () => {
   window.addEventListener("scroll", function () {
