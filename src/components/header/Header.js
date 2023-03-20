3ffc448e69d48720dd4da511f4a39b632df64107
David Na <hna206@naver.com>
2023-03-20 14:15:25 +1300

    hdeader changed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,14 @@ import "./Header.css";
 
 
 const Header = () => {
+  /*=============== Change Background Header ===============*/
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    // when the scroll is higher than 200 viewport height, add the scroll-header class to a tag with the header tag
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
+
 
   const [toggle, showMenue] = useState(false);
 
