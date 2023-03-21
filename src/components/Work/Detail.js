dcdb042bb1842121aa282b4131efaa75216f6708
David Na <hna206@naver.com>
2023-03-22 00:57:40 +1300

    modal test

---
+++ src/components/Work/Detail.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const Detail = () => {
+    const [toggle, setToggle] = useState(0);
+
+    const toggleTab = (index) => {
+      setToggle(index);
+    };
+  
+
+  return (
+    <>
+              <span className="services__button" onClick={() => toggleTab(1)}>
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div
+            className={
+              toggle === 1
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">Web Application</h3>
+              <p className="services__modal-description">
+                  The field that I have been studying <br />for over 6 months.
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I deveop Single-Page Application(SPA)
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web page development.</p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Simple Emotional Diary
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Deploying via Firebase || Github page
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+    </>
+  )
+}
+
+export default Detail
--- src/components/Work/WorksItems.js
+++ src/components/Work/WorksItems.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import Detail from "./Detail";
 
 const WorkItems = ({ item }) => {
+
+  const [toggle, setToggle] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggle(index);
+  };
+
+  
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
-        More
-        <i className="bx bx-right-arrow-alt work__button-icon"></i>
-      </a>
+        <Detail />
+        {/* <i className="bx bx-right-arrow-alt work__button-icon"></i> */}
     </div>
   );
 };
