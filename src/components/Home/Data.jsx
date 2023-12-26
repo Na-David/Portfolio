f4fbafb96720dbc31717ba0fe2e36b054712d7e5
David Na <hna206@naver.com>
2023-12-26 22:13:30 +1300

    CV changed

--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Data = () => {
   return (
-    <div className='home__data'>
-      <h1 className='home__title'>David Na</h1>
-      <h3 className='home__subtitle'>AUT BCIS student<br />Software Development</h3>
-      <p className='home__description'> 
-        I am a student who has regularly worked with React.js and C# in learning software development.
-        I enjoy programming and have a passion for solving problems with perseverance. 
-        Furthermore, I strongly desire to learn new technologies and am highly motivated to put forth the effort required to learn. 
+    <div className="home__data">
+      <h1 className="home__title">David Na</h1>
+      <h3 className="home__subtitle">
+        AUT BCIS <br />
+        Software Development
+      </h3>
+      <p className="home__description">
+        I am a student who has regularly worked with React.js and C# in learning
+        software development. I enjoy programming and have a passion for solving
+        problems with perseverance. Furthermore, I strongly desire to learn new
+        technologies and am highly motivated to put forth the effort required to
+        learn.
       </p>
 
-      <a href='#contact' className='button button--flex'>
-        Say Hello　<i className='uil uil-message nav__icon'></i>
+      <a href="#contact" className="button button--flex">
+        Say Hello　<i className="uil uil-message nav__icon"></i>
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default Data
+export default Data;
